58b9d3efa4c4e7a5455f168deb8ef3320c5cb05e
Umakhihe Arnold <umakhihe.arnold@branchd.ng>
2020-02-17 02:57:46 +0000

    chapter 10

--- rango/forms.py
+++ rango/forms.py
@@ -43,10 +43,37 @@ class PageForm(forms.ModelForm):
 		
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
-	class Meta: model = User
-    	fields = ('username', 'email', 'password',)
+	
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password',)
 
 class UserProfileForm(forms.ModelForm):
 	class Meta:
-    	model = UserProfile
-    	fields = ('website', 'picture',)
+		model = UserProfile
+		fields = ('website', 'picture',)
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
